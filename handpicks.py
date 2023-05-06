0c39d2256745e9b153a3337d11b717fc88abd007
Hextropian Systems, LLC <hextropian@hextropian.systems>
2023-05-06 17:55:05 +0000

    Added 1 symbol to exclusion list

--- handpicks.py
+++ handpicks.py
@@ -1,2 +1,2 @@
-exclusion=['CLR', 'ZEN', 'SWCH', 'STOR', 'COUP', 'IAA', 'SGFY']
+exclusion=['CLR', 'ZEN', 'SWCH', 'STOR', 'COUP', 'IAA', 'SGFY', 'OSH']
 inclusion=['SPY', '^DJI', '^GSPC', '^IXIC', '^NYA', '^RUI', 'BIL']
